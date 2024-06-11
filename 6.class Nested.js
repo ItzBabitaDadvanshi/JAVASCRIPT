fee1c91a3273bf427417c5b3e2bf10ee9239da3f
unknown <jaatl6675@gmail.com>
2024-06-11 12:23:17 +0530

    10 june

---
+++ 6.class Nested.js
@@ -0,0 +1,21 @@
+var stu_name="Babita";
+var matrix_12=65;
+var stream="PCM";
+var entrance_exam="pass";
+var admission="completed";
+
+var stu_name="Babita"
+if(stu_name){
+    console.log(stu_name+" has applied for admission")
+
+    var matrix_12=65;
+    if(matrix_12>=70){
+        console.log(stu_name+" has passed  12th class ")
+    }
+    else{
+        console.log(stu_name+" has not passed 12th class")
+    }
+}
+else{
+    console.log(stu_name+" has not eligible for admission")
+}
