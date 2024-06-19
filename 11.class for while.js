3759d724c9bb35a5e6798dbd4e46f4fb35f376ef
ItzBabitaDadvanshi <jaatl6675@gmail.com>
2024-06-19 12:58:24 +0530

    javascript Class

---
+++ 11.class for while.js
@@ -0,0 +1,32 @@
+// Initialization 
+let a=0;
+while (a<=10){
+    console.log("Dreamer infotech",a)
+    a++; 
+    // increamenter//decreamenter
+} 
+
+let b=1;
+while (a<10){
+    a++;
+    console.log("Hello",b)
+}
+
+let c=1;
+while(c<10){
+    if(c==3){
+        break;
+    }
+    console.log(c);
+    c++;
+}
+
+let d=1;
+while(d<10){
+    if(d==4){
+        d++;
+        continue;
+    }
+    console.log(d);
+    d++;
+}
